6567135bfa6e6f6e7b688e3380371354a2b30cda
msantam2 <msantam2@gmail.com>
2018-09-27 11:20:50 -0700

    Initial setup (main UI: labels, constraints, outlet connections)

--- gimbal-app/ViewController.h
+++ gimbal-app/ViewController.h
@@ -9,6 +9,9 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *latitudeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *longitudeLabel;
+
 
 
 @end
